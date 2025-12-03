e83d1c594f4dee0610e8dfb7dae008b34414954a
barath-16 <64786709+barath-16@users.noreply.github.com>
2025-12-03 17:22:57 +0530

    Add files via upload
    Home Assignment - Week 1 Day 1 - Barath R K

---
+++ week1/day1/EmployeeDetails.java
@@ -0,0 +1,29 @@
+package week1.day1;
+
+public class EmployeeDetails {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		String empName = "John Doe";
+		byte empAge = 30;
+		float empSalary = 55000.75f;
+		char empGender = 'M';
+		boolean acceptTerms = true;
+		float empRating = 4.5f;
+		long mobileNumber = 9876543210l;
+		int pinCode = 56001;
+		byte luckyNumber = 7;
+		
+		System.out.println("Employee name :" +" " + empName);
+		System.out.println("Employee age :" +" " + empAge);
+		System.out.println("Employee salary :" +" " + empSalary);
+		System.out.println("Employee gender :" +" " + empGender);
+		System.out.println("Terms and Conditions : " +" " + acceptTerms);
+		System.out.println("Employee rating : "+ " " +empRating);
+		System.out.println("Employee mobile number : "+ " " +mobileNumber);
+		System.out.println("Pincode :"+ pinCode);
+		System.out.println("Employee's lucky number :"+" " + luckyNumber);
+	}
+
+}
---
+++ week1/day1/LearnDataTypes.java
@@ -0,0 +1,17 @@
+package week1.day1;
+
+public class LearnDataTypes {
+	
+	public static void main(String[] args) {
+		
+		int month = 12;
+		String monthName = "DECEMBER";
+		boolean currentMonth = true;
+		char day = 'W';
+		float decimals = 1.09309f;
+		
+		System.out.println(month+" "+ monthName+" "+ currentMonth+ " " + day+" "+ decimals);
+		
+	}
+
+}
